d100e362e193d2b1882b8934323cd93014f58b53
Konrad Malawski <konrad.malawski@project13.pl>
2011-03-30 16:51:35 +0200

    yay scala keywords

--- src/main/scala/pl/project13/protoscala/utils/ScalaNameMangler.scala
+++ src/main/scala/pl/project13/protoscala/utils/ScalaNameMangler.scala
@@ -9,8 +9,17 @@ import java.lang.String
  */
 class ScalaNameMangler {
 
-  val scalaKeywords = "type" :: "class" :: "var" :: "val" :: "int" :: "double" ::
-    "trait" :: "return" :: "implements" :: Nil
+  val scalaKeywords = "abstract" :: "do"        :: "finally"  :: "import"  :: "object"    ::
+                      "return"   :: "trait"     :: "var"      :: "_"       :: ":"         ::
+                      "case"     :: "else"      :: "for"      :: "="       :: "=>"        ::
+                      "lazy"     :: "override"  :: "sealed"   :: "try"     :: "while"     ::
+                      "package"  :: "super"     :: "if"       :: "new"     :: "private"   ::
+                      "<-"       :: "catch"     :: "extends"  :: "forSome" :: "match"     ::
+                      "true"     :: "with"      :: "<:"       :: "class"   :: "false"     ::
+                      "this"     :: "type"      :: "yield"    :: "<%"      :: ">:"        ::
+                      "def"      :: "final"     :: "implicit" :: "null"    :: "protected" ::
+                      "throw"    :: "val"       :: "#"        :: "@"       :: Nill
+
 
   val fileNameExt = ".scala"
 
@@ -26,7 +35,7 @@ class ScalaNameMangler {
     possiblyUnsafeFilename + fileNameExt
   }
 
-  private def mangle(possiblyUnsafeName: String): String = {
+  def mangle(possiblyUnsafeName: String): String = {
     possiblyUnsafeName + "_"
   }
 }
